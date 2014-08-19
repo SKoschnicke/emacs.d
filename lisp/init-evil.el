e5a3a8497661add66d5b10a32ca72bd4e362f2b3
Sven Koschnicke <s.koschnicke@gfxpro.com>
2014-08-19 09:35:23 +0200

    Added evil-surround

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -2,5 +2,10 @@
   (require-package 'evil)
   (add-hook 'after-init-hook 'evil-mode))
 
+(after-load 'evil
+  (require-package 'evil-surround)
+  (global-evil-surround-mode 1)
+)
+
 (provide 'init-evil)
 ;;; init-evil.el ends here
