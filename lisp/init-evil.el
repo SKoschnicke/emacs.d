08be8eb4757a3969c92faf572cd4271ffb974bd3
Sven Koschnicke <s.koschnicke@gfxpro.com>
2014-11-18 11:00:57 +0100

    Reenable universal argument, bind scroll up to C-y

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,5 +1,3 @@
-(setq evil-want-C-u-scroll t) ;; bind C-u to scroll-up (as in vi)
-
 (when (eval-when-compile (>= emacs-major-version 24))
   (require-package 'evil)
   (add-hook 'after-init-hook 'evil-mode))
@@ -7,6 +5,13 @@
 (after-load 'evil
   (require-package 'evil-surround)
   (global-evil-surround-mode 1)
+  ;(setq evil-want-C-u-scroll t) ;; bind C-u to scroll-up (as in vi)
+  ; Use C-y for scrolling up (its C-u in vim but thats bound to universal argument in emacs)
+  (define-key evil-normal-state-map "\C-y" (lambda ()
+                    (interactive)
+                    (previous-line nil)
+                    (evil-scroll-line-up nil)
+                    ))
 )
 
 (provide 'init-evil)
